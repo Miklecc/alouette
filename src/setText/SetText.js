84127156a3aff0775851b8dc2b654bb83a85400f
Miklecc <mikle.blv@gmail.com>
2017-06-08 21:42:09 +0300

    - updated template and styling of `setText` component

--- src/setText/SetText.js
+++ src/setText/SetText.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchSetText, setTextSuccess } from './setTextActions'
-import { Form, Icon, Input, Button, Row, Col } from 'antd'
+import { Form, Icon, Input, Button } from 'antd'
 
 const FormItem = Form.Item;
 
@@ -31,62 +31,38 @@ class SetTextTemplate extends React.Component {
 
         return (
             <div>
-                <Row>
-                    <Col className="setText-background-image-container" span={24}>
-                        <img className="setText-background-image"
-                            src={''}
-                            alt="Poehali!" />
-                    </Col>
-                </Row>
-                <Row className="gutter-row" gutter={40}>
-                    <Col span={10}>
-                        <Form layout="inline" onSubmit={this.handleSubmit} className="setText-form">
-                            Privet ot Alouette
+                <Form layout="inline" onSubmit={this.handleSubmit} className="setText-form">
+                    Privet ot Alouette
                             <hr className="setText-divider" />
 
-                            <FormItem
-                                label='Enter your phrase'
-                                colon={false}
-                                validateStatus={setTextFieldError ? 'error' : ''}
-                                help={setTextFieldError || ''}>
-                                {getFieldDecorator('setTextField', {
-                                    rules: [{
-                                        required: false,
-                                        message: 'Please input text with rule [1-10]',
-                                        pattern: /^([1-9]|10)$/g
-                                    }],
-                                    initialValue: props.setTextReducer.enteredText
-                                })(
-                                    <Input
-                                        prefix={<Icon type="aliwangwang-o"
-                                        style={{ fontSize: 13 }} />}
-                                        placeholder="Text to process" />
-                                    )}
-                            </FormItem>
-                            <FormItem
-                                className="setText-entered-text"
-                                label='Entered text is'
-                                colon={false}>
-                                <Input
-                                    value={props.setTextReducer.enteredText}
-                                    className="setText-entered-text-input" />
-                            </FormItem>
-                            <FormItem>
-                                <Button
-                                    type="primary"
-                                    htmlType="submit"
-                                    disabled={hasErrors(getFieldsError())}>
-                                    Log in
-                                 </Button>
-                            </FormItem>
-                        </Form>
-                    </Col>
-                    <Col span={12}></Col>
-                </Row>
-                <Row className="gutter-row" gutter={40}>
-                    <Col span={10}></Col>
-                    <Col span={12}></Col>
-                </Row>
+                    <FormItem
+                        label='Enter phrase'
+                        colon={false}
+                        validateStatus={setTextFieldError ? 'error' : ''}
+                        help={setTextFieldError || ''}>
+                        {getFieldDecorator('setTextField', {
+                            rules: [{
+                                required: false,
+                                message: 'Please input text with rule [1-10]',
+                                pattern: /^([1-9]|10)$/g
+                            }],
+                            initialValue: props.setTextReducer.enteredText
+                        })(
+                            <Input
+                                prefix={<Icon type="aliwangwang-o"
+                                    style={{ fontSize: 13 }} />}
+                                placeholder="Text to process" />
+                            )}
+                    </FormItem>
+                    <FormItem
+                        className="setText-entered-text"
+                        label='Your phrase is'
+                        colon={false}>
+                        <Input
+                            value={props.setTextReducer.enteredText}
+                            className="setText-entered-text-input" />
+                    </FormItem>
+                </Form>
             </div>
         );
     }
@@ -97,7 +73,6 @@ function mapStateToProps(state) {
     // console.log('state ++++ ', state);
     // TODO: object setTextReducer and appData comes -> nope, it's fine to have reducers for each component and 'combineReducers' joins them
     // TODO: -> SKIPPED observable to setText to parse/map/etc to show autocomplete in search
-    // TODO: styling -> connect to backend
     return {
         setTextReducer: state.setTextReducer
     }
